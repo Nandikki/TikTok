4d42c5cc46b2b9dc359f0af2bc8ad0fb10e4ee8a
Abel fernando Barros rodrigues <abarros-@c1r8s1.42lisboa.com>
2021-04-28 21:02:41 +0100

    3h

--- tik-tok-clone/src/App.js
+++ tik-tok-clone/src/App.js
@@ -1,21 +1,28 @@
+import React, { useState, useEffect } from "react";
 import './App.css';
 import Video from './Video';
+import db from "./firebase";
 
 function App() {
+  const [videos, setVideos] = useState([])
+  useEffect(() => {
+    db.collection('videos').onSnapshot(snapshot => setVideos(snapshot.docs.map(doc => doc.data())))
+   }, [videos])
   return (
     <div className="app">
       <div className="app__videos">
-        <Video channel="bellapoarch" 
-        description="It's my time bro! eheh #LetsGo!" 
-        song="Lil Nas X | call me when you want call me when you need"
-        likes={8918}
-        comments={987}
-        shares={534}
-        src="https://v77.tiktokcdn.com/37e6db03a1e00d5dcb557a4801331022/6089e039/video/tos/useast2a/tos-useast2a-ve-0068c004/09027a84b36349b19838ebef4b441d59/?a=1233&br=1740&bt=870&cd=0%7C0%7C1&ch=0&cr=0&cs=0&cv=1&dr=0&ds=3&er=&l=20210428162237010189072032460EA5B4&lr=tiktok_m&mime_type=video_mp4&net=0&pl=0&qs=0&rc=M3Bsd3lpZjV4NDMzNzczM0ApPDY7NTkzPDxoNzlmZzg1OWdlYWhsYV4wX2hgLS1kMTZzcy8tMmBfM2JfXjIzLzFeNDM6Yw%3D%3D&vl=&vr="/>
-        <Video />
-        <Video />
-        <Video />
-      </div>
+        {videos.map(
+          ({url, channel, description, song, likes, comments, shares}) => (
+          <Video url={url}
+          song ={song}
+          likes = {likes}
+          comments = {comments}
+          shares = {shares}
+          channel = {channel}
+          description = {description}/>
+          )
+        )}
+        </div>
     </div>
     
   );
--- tik-tok-clone/src/firebase.js
+++ tik-tok-clone/src/firebase.js
@@ -1,3 +1,4 @@
+import firebase from "firebase"
 const firebaseConfig = {
     apiKey: "AIzaSyBoaq9OHXg4HJ14DDZodItcpfxg2oCrQTQ",
     authDomain: "tik-tok-clone-nandikki.firebaseapp.com",
@@ -6,3 +7,9 @@ const firebaseConfig = {
     messagingSenderId: "923905406089",
     appId: "1:923905406089:web:05fac387a7e2b7a516c258"
   };
+
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+  const db = firebaseApp.firestore();
+
+  export default db;
